7cb7748f67da9962e985b8e085d96f801730c3e4
Ahmadjon Mustafoyev <mustafayevaxmadjon@gmail.com>
2023-06-16 22:53:17 +0500

    Edit Profile

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,15 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, Put } from '@nestjs/common';
 import { Auth } from 'src/auth/decorators/auth.decorator';
 import { User } from './decorators/user.decorator';
 import { UserService } from './user.service';
+import { InterfaceEmailAndPassword } from './user.interface';
 
 @Controller('user')
 export class UserController {
     constructor(private readonly userService: UserService) { }
 
+    @HttpCode(200)
     @Get('profile')
     @Auth()
     async getProfile(@User('_id') _id: string) {
         return this.userService.byId(_id);
+    }
+
+    @HttpCode(200)
+    @Put("edit-password")
+    async editPassword(@Body() dto: InterfaceEmailAndPassword) { 
+        return this.userService.editPassword(dto)
     }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,7 +1,9 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User, UserDocument } from './user.model';
+import { InterfaceEmailAndPassword } from './user.interface';
+import { genSalt, hash } from 'bcryptjs';
 
 @Injectable()
 export class UserService {
@@ -13,5 +15,15 @@ export class UserService {
         if (!user) throw new NotFoundException('User not found');
 
         return user;
+    }
+
+    async editPassword(dto: InterfaceEmailAndPassword) {
+        const { email, password } = dto
+        const existUser = await this.userModel.findOne({ email })
+        if (!existUser) throw new UnauthorizedException("User not found")
+        const salt = await genSalt(10)
+        const hashPassword = await hash(password, salt)
+        await this.userModel.findByIdAndUpdate(existUser._id, { $set: { password: hashPassword } }, { new: true })
+        return "success"
     }
 }
